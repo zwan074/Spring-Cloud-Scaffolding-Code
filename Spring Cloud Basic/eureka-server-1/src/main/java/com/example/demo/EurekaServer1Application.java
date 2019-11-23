2ee5fd238bf9b8811616a8d9eea9612063b2c95a
zwan074 <42155686+zwan074@users.noreply.github.com>
2019-11-24 11:14:39 +1300

    Add files via upload

---
+++ Spring Cloud Basic/eureka-server-1/src/main/java/com/example/demo/EurekaServer1Application.java
@@ -0,0 +1,15 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer // EurekaServer server starter, accept other nodes registrations
+public class EurekaServer1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EurekaServer1Application.class, args);
+	}
+
+}
